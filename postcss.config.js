af79d55aa58f8b024171ccf9a27743adb18101b5
MazTub <thomas.mazhar-elstub@paconsulting.com>
2025-04-24 11:26:05 +0100

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/*  ES-module style  */
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+    plugins: [
+      tailwindcss,
+      autoprefixer
+    ],
+  };
+  
